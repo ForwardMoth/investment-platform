cc3b09fee0b6a00a14af8336d169f6de84725236
ForwardMoth <Budancev-a-a1@yandex.ru>
2023-12-10 16:58:49 +0300

    fix: handling security exception

--- src/main/java/com/example/controller/handler/SecurityExceptionHandler.java
+++ src/main/java/com/example/controller/handler/SecurityExceptionHandler.java
@@ -1,26 +1,26 @@
 package com.example.controller.handler;
 
-import com.example.exception.message.SecurityErrorMessage;
 import com.example.exception.SecurityException;
+import com.example.exception.message.ErrorMessage;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @Slf4j
 @RestControllerAdvice
-public class SecurityExceptionHandler extends ResponseEntityExceptionHandler {
+public class SecurityExceptionHandler {
     @ExceptionHandler(SecurityException.class)
-    public ResponseEntity<SecurityErrorMessage> handleSecurityException(SecurityException e) {
+    public ResponseEntity<ErrorMessage> handleSecurityException(SecurityException e) {
         SecurityException.CODE code = e.getCode();
         HttpStatus status = switch (code) {
             case TICKER_IN_USE -> HttpStatus.CONFLICT;
         };
         String codeStr = code.toString();
+        log.error(e.getMessage());
         return ResponseEntity
                 .status(status)
-                .body(new SecurityErrorMessage(codeStr, e.getMessage()));
+                .body(new ErrorMessage(codeStr, e.getMessage()));
     }
 }
